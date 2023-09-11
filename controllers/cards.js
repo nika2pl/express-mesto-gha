e8960d30a8fb3c2f010e7a5ca41110c6a43adbc0
Nikita Plokhikh <nika8123@ya.ru>
2023-09-11 22:46:50 +0300

    Исправления после первого ревью

--- controllers/cards.js
+++ controllers/cards.js
@@ -13,7 +13,7 @@ module.exports.getCards = (req, res) => {
 };
 
 module.exports.deleteCard = (req, res) => {
-  Card.findByIdAndRemove({ _id: req.params.cardId })
+  Card.findByIdAndRemove({ _id: req.params.cardId }).orFail()
     .then((data) => (
       !data
         ? res.status(ERROR_NOT_FOUND).send({ message: 'Карточка не найдена' })
@@ -43,7 +43,7 @@ module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $addToSet: { likes: req.user._id } },
   { new: true },
-).then((data) => res.status(201).send(data)).catch((err) => (
+).orFail().then((data) => res.status(201).send(data)).catch((err) => (
   err instanceof mongoose.Error.DocumentNotFoundError
     ? res.status(ERROR_INCORRECT_DATA).send({ message: 'Карточка не найдена' })
     : res.status(ERROR_INTERNAL_SERVER).send({ message: `Произошла ошибка: ${err.message}` })
@@ -53,7 +53,7 @@ module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $pull: { likes: req.user._id } },
   { new: true },
-).then((data) => res.send(data)).catch((err) => (
+).orFail().then((data) => res.send(data)).catch((err) => (
   err instanceof mongoose.Error.DocumentNotFoundError
     ? res.status(ERROR_INCORRECT_DATA).send({ message: 'Карточка не найдена' })
     : res.status(ERROR_INTERNAL_SERVER).send({ message: `Произошла ошибка: ${err.message}` })
